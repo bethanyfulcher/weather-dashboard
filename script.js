453cdf9744c060866394c630ed25368986be345e
Bethany Fulcher <fulcherbethany@gmail.com>
2020-12-20 12:09:32 -0800

    added skeleton and styling

---
+++ script.js
@@ -0,0 +1,32 @@
+// TODO: when button is pushed
+//  TODO: fetch user input
+//  TODO: save into local storage (set)
+//  TODO: put all searched items from local storage in .search
+//      TODO: make sure to save to a global variable to use for the forecast
+
+
+// TODO: create variable named city that is equal to user input
+// TODO: create variable named queryURL that is equal to the open weather map URL
+//      TODO: replace the city name to the variable named city
+// TODO: use AJAX to retrieve open weather map object for today's date
+//  TODO: replace text of #city with results.data.city
+//      TODO: replace text of #todaysDate with results.data.date
+//      TODO: replace text of #todaysIcon with results.data.icon
+//  TODO: replace text of #temperature with results.data.temperature
+//  TODO: replace text of #humidity with results.data.humidity
+//  TODO: replace text of #windspeed with results.data.windspeed
+//  TODO: replace text of #UVindex with results.data.uvindex
+//      TODO: change UVindex background based on the results
+//          TODO: if UVindex is less than 3, then set background color to green
+//          TODO: if UVindex is greater than or equal to 3 and less than 6, then set background color to yellow
+//          TODO: if UVindex is greater than or equal to 6, then set background color to red
+
+
+//  TODO: within another AJAX, retrieve open weather map object for the next five days
+//      TODO: do this by placing the AJAX inside a for loop
+//      TODO: the for loop should loop 5 times, each time producing the next day and placing the data into a different slot in the forecast area
+//    TODO: find element with value of i
+//    TODO: replace text of .date with results.data.date
+//    TODO: replace text of .icon with results.data.icon
+//    TODO: replace text of .temperature with results.data.temperature
+//    TODO: replace text of .humidity with resulst.data.humidity
