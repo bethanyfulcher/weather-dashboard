853076d7272bb3522e41623654a69bb99c96e4fa
Bethany Fulcher <fulcherbethany@gmail.com>
2020-12-21 09:53:19 -0800

    updated forecast to show correct data

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var searches = [];
-
+var lon;
+var lat;
 // fetches all previous searches and places them on page
 init();
 
@@ -27,44 +28,101 @@ $("#searchBtn").on("click", function() {
     var city = userInput;
     // create variable named queryURL that is equal to the open weather map URL
     //      replace the city name to the variable named city
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=09746f545308cabfeaaf4a81e5a0da1d
+    var locationURL = `https://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=09746f545308cabfeaaf4a81e5a0da1d
     `
-    // TODO: use AJAX to retrieve open weather map object for today's date
+    // use AJAX to retrieve open weather map object for longitude and latitude
     $.ajax({
-        url: queryURL,
+        url: locationURL,
         method: "GET"
     }).then(function(result) {
-        console.log(result)
-    });
-    //  TODO: replace text of #city with results.data.city
-    //      TODO: replace text of #todaysDate with results.data.date
-    //      TODO: replace text of #todaysIcon with results.data.icon
-//  TODO: replace text of #temperature with results.data.temperature
-//  TODO: replace text of #humidity with results.data.humidity
-//  TODO: replace text of #windspeed with results.data.windspeed
-//  TODO: replace text of #UVindex with results.data.uvindex
-//      TODO: change UVindex background based on the results
-//          TODO: if UVindex is less than 3, then set background color to green
-//          TODO: if UVindex is greater than or equal to 3 and less than 6, then set background color to yellow
-//          TODO: if UVindex is greater than or equal to 6, then set background color to red
+        // console.log(result)
+        //  replace text of #city with results.data.city
+        $("#city").text(result.name)
 
+        lat = result.coord.lat;
+        lon = result.coord.lon;
+        // console.log(lat)
+        // console.log(lon)
+        // use another Ajax to retrieve open weather map object for all data
+        var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=09746f545308cabfeaaf4a81e5a0da1d`;
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
 
-//  TODO: within another AJAX, retrieve open weather map object for the next five days
-//      TODO: do this by placing the AJAX inside a for loop
-//      TODO: the for loop should loop 5 times, each time producing the next day and placing the data into a different slot in the forecast area
-//    TODO: find element with value of i
-//    TODO: replace text of .date with results.data.date
-//    TODO: replace text of .icon with results.data.icon
-//    TODO: replace text of .temperature with results.data.temperature
-//    TODO: replace text of .humidity with resulst.data.humidity
+            //      TODO: replace text of #todaysDate with results.data.date
+            //      TODO: replace text of #todaysIcon with results.data.icon
+            $("#icon")
+            //  TODO: replace text of #temperature with results.data.temperature
+            $("#temperature").text(response.current.temp);
+            //  replace text of #humidity with results.data.humidity
+            $("#humidity").text(response.current.humidity);
+            //  replace text of #windspeed with results.data.windspeed
+            $("#windspeed").text(response.current.wind_speed);
+            //  replace text of #UVindex with results.data.uvindex
+            var UVindex = response.current.uvi;
 
-});
+            // $("#UVindex").css("");
+            $("#UVindex").text(UVindex);
+            //      change UVindex background based on the results
+            //          if UVindex is less than 3, then set background color to green
+            if (UVindex < 3)
+            {
+                $("#UVindex").css("background-color", "green");
+            }
+            //          if UVindex is greater than or equal to 6, then set background color to red
+            else if (UVindex >= 6)
+            {
+                $("#UVindex").css("background-color", "red");
+            }
+            //          if UVindex is greater than or equal to 3 and less than 6, then set background color to yellow
+            else 
+            {
+                $("UVindex").css("background-color", "yellow");
+            }
+            
+            //      TODO: the for loop should loop 5 times, each time producing the next day and placing the data into a different slot in the forecast area
+            $("#forecast").text("");
+            for (var i = 1; i < 6; i++)
+            {
+                var div = $("<div>");
+                div.addClass("col-2 bg-primary rounded justify-content-evenly");
+                $("#forecast").append(div);
+                
+                //    TODO: replace text of .date with results.data.date
+                var date = $("<p>");
+                date.text("date");
+                date.addClass("dateText")
+                div.append(date);
+                
+                //    TODO: replace text of .icon with results.data.icon
+                var icon = $("<img>");
+                icon.attr("src", `http://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}.png`);
+                div.append(icon);
+                
+                //    TODO: replace text of .temperature with results.data.temperature
+                var temp = $("<p>");
+                temp.text(response.daily[i].temp.day);
+                div.append(temp);
 
-function renderSearches() {
-    $("#searches").html("");
+                //    TODO: replace text of .humidity with resulst.data.humidity
+                var humidity = $("<p>");
+                humidity.text(response.daily[i].humidity);
+                div.append(humidity);
+            }
 
-    for (var i = 0; i < searches.length; i++)
-    {
+        })
+        });
+            
+        });
+        
+        function renderSearches() {
+            $("#searches").html("");
+            
+            for (var i = 0; i < searches.length; i++)
+            {
         var search = searches[i];
 
         var li = $("<li>");
